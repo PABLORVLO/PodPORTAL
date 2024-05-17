0f7400848476e4203c3e18c6425e40ce1f6cb70f
PABLORVLO <pablorevelo209@gmail.com>
2024-05-17 13:19:47 -0500

    proyecto

---
+++ client/src/utils/Themes.js
@@ -0,0 +1,19 @@
+export const darkTheme = {
+    bg: "#080202",
+    bgLight: "#080202",
+    primary: "#FCFCFC",
+    text_primary: "#F2F3F4",
+    text_secondary: "#B1B2B3",
+    card: "#070707",
+    button: "#5C5B5B",
+}
+
+export const lightTheme = {
+    bg: "#FCFCFC",
+    bgLight: "#F0F0F0",
+    primary: "#BE1ADB",
+    text_primary: "#111111",
+    text_secondary: "#48494A",
+    card: "FCFCFC",
+    button: "#5C5B5B",
+}
